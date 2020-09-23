dd06f64b97376694706762d46f7caff79013d814
Don Schniitzius <don@sketchbookclub.com>
2020-09-22 22:53:57 -0500

    Version 1.0

--- resize_node.py
+++ resize_node.py
@@ -22,7 +22,7 @@ bl_info = {
     "version": (1, 0),
     "blender": (2, 80, 0),
     "location": "Node Editor > Sidebar > Arrange",
-    "description": "Assign a Fixed Width to an Active Node",
+    "description": "Assign a Fixed Width to an Active Node in Shader Editor",
     "warning": "",
     "doc_url": "",
     "category": "Node",
